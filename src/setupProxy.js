d4f64759060ea5f538b10b88f2667866bbe8c3f9
Mango Juicy <134001432+Mango-Juicy@users.noreply.github.com>
2023-07-27 21:25:32 +0200

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
-        '/api',
+        ['/images','/api'],
         createProxyMiddleware({
             target: 'http://3.72.53.53:8000',
             changeOrigin: true,
